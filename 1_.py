bd86bb0db14338c76396ef60b1d64f8f333ffc96
Starrbr1ght <alaynabentley3@gmail.com>
2026-01-27 14:34:12 +0000

    IDK

--- 1_.py
+++ 1_.py
@@ -6,8 +6,59 @@
 # :
 
 # %
+#function = a block of reusable code
+#                  place () after the function nakme to invoke it
+def happy_birthday(age, name): #adding name into the parenthesis is called a peramiter
+    print(f"Happy birthday to {name}")
+    print(f"you are {age} years old!")
+    print("happy birthday to you!")
+    print()
 
-# _
+happy_birthday(20, steve)
+
+
+
+def display_invoice(username, amount, due_date):
+    print(f"hello {username}")
+    print(f"your bill of ${amount:.2f} is due: {due_date}")
+
+
+
+display_invoice()
+
+
+
+    
+
+
+
+def add(x, y):
+    z= x + y
+    return z
+
+
+def subtract(x, y):
+    z= x - y 
+    return z
+
+def multiply(x,y):
+    z= x * y
+    return z
+
+def divide(x, y):
+    z= x / y
+    return z
+
+print(add(1, 2))
+print(subtract(1, 2))
+print(multiply(1, 2))
+print(divide(1, 2))
+
+
+def create_name (first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
 
 # #
 
